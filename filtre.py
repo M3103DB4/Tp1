ca4f2f1662944e779d515f0b5563be1285aac57e
DURVILLE <fd89400@gmail.com>
2017-03-24 10:14:28 +0100

    Add files via upload

--- filtre.py
+++ filtre.py
@@ -4,21 +4,21 @@ serverPortIN = 12000
 serverPortOUT = 13000
 serverName = "localhost"
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPortOUT))
 
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPortIN))
-serverSocket.listen(1)
-
+serverSocket1 = socket(AF_INET,SOCK_STREAM)
+serverSocket2 = socket(AF_INET,SOCK_STREAM)
+serverSocket1.bind(('',serverPortIN))
+serverSocket2.bind(('',serverPortOUT))
+serverSocket1.listen(1)
+serverSocket2.listen(1)
 while 1:
-	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024)
-	modifiedSentence = sentence.upper()
-	clientSocket.send(modifiedSentence)
-	connectionSocket.close()
-
-clientSocket.close()
-
-   
+	connectionSocket1, addr = serverSocket1.accept()
+	sentence1 = connectionSocket1.recv(1024)
+	modifiedSentence = sentence1.upper()
+	connectionSocket1.send(modifiedSentence)
+	connectionSocket1.close()
+	connectionSocket2, addr = serverSocket2.accept()
+	connectionSocket2.send(modifiedSentence)
+	connectionSocket2.close()
+	print "reussir"
 
